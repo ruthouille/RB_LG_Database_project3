569eb8af51f5a15126aa5c2972fd6ffcf36a8572
Ubuntu <ubuntu@ip-172-31-37-222.eu-west-1.compute.internal>
2022-11-14 23:31:45 +0000

    creatioon de la table (csv to db)

---
+++ my_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+import pandas as pd
+
+
+conn = sqlite3.connect('my_dabase.db')
+c = conn.cursor()
+
+c.execute('''DROP TABLE IF EXISTS my_dataset''')
+c.execute('''CREATE TABLE my_dataset (show_id String, type String, title String, director String, cast String, country String,
+date_added String, release_year Integer, rating String, duration String, listed_in String, description String)''')
+
+#Load our file
+my_dataset = pd.read_csv('netflix_titles.csv')
+
+# write the data to a sqlite table
+my_dataset.to_sql('my_dataset', conn, if_exists='append', index = False)
+
+print(c.execute('''SELECT * FROM my_dataset LIMIT 10''').fetchall())
+
+#L'API va devoir prendre en argument un texte qui va etre lu et executer (le texte sera notre requête)
