89a1f78ad2538a76251bb70e7fe645104ba87fed
Ubuntu <ubuntu@ip-172-31-37-222.eu-west-1.compute.internal>
2022-11-21 00:17:49 +0000

    modifications

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+mkdir db_api
+mv db_api.py db_api
+mv my_db.py db_api
+mv requirements.txt db_api
+mv Dockerfile db_api
+
+cd db_api
 docker image build . -t db_api:1.0.0
-docker-compose up -d
+
+cd ../
+docker-compose up
